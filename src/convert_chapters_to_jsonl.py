349dc169f13a87c26a7782891c6e81d67724abcb
Maggie Mao <guanqunm@gmail.com>
2023-07-15 14:29:21 -0700

    Update convert jsonl to batch process directories

--- src/convert_chapters_to_jsonl.py
+++ src/convert_chapters_to_jsonl.py
@@ -2,22 +2,29 @@ import os
 import pathlib
 import json
 
-CHAPTERS_OUTPUT_DIR=pathlib.Path(__file__).parent.parent / "data" / "chapters" / "367_Sam_Altman_L_Guz73e6fw"
-JSONL_OUTPUT_PATH=pathlib.Path(__file__).parent.parent / "data" / "finetune_dataset" / "367_Sam_Altman_L_Guz73e6fw"
+DIR_NAMES = ["367_Sam_Altman_L_Guz73e6fw", "386_Marc_Andreessen"]
+CHAPTERS_DIR_PREFIX = pathlib.Path(__file__).parent.parent / "data" / "chapters"
+JSONL_OUTPUT_DIR_PREFIX = pathlib.Path(__file__).parent.parent / "data" / "finetune_dataset" / "LexFridman"
 
 
 def main():
-    os.makedirs(JSONL_OUTPUT_PATH, exist_ok=True)
-    jsonl_lines: list[str] = []
+    os.makedirs(JSONL_OUTPUT_DIR_PREFIX, exist_ok=True)
 
-    for root, dirs, files in os.walk(CHAPTERS_OUTPUT_DIR):
-        for file in files:
-            chapter_name = file.split("__")[1].split(".txt")[0]
-            content = open(os.path.join(root, file)).read()
-            jsonl_lines.append(json.dumps({"pod": "Lex Friedman Podcast", "input": chapter_name, "output": content}) + "\n")
+    for dir_name in DIR_NAMES:
 
-    with open(JSONL_OUTPUT_PATH / "data.json", 'w') as fh:
-        fh.writelines(jsonl_lines)
+        jsonl_lines: list[str] = []
+        dir_path = CHAPTERS_DIR_PREFIX / dir_name
+        for root, dirs, files in os.walk(dir_path):
+            # sort by chronological order for debugging purpose
+            files.sort(key=lambda x: int(x.split("__")[0]))
+
+            for file in files:
+                chapter_name = file.split("__")[1].split(".txt")[0]
+                content = open(os.path.join(root, file)).read()
+                jsonl_lines.append(json.dumps({"pod": "Lex Fridman Podcast", "input": chapter_name, "output": content}) + "\n")
+
+        with open(JSONL_OUTPUT_DIR_PREFIX / (dir_name + ".json"), 'w') as fh:
+            fh.writelines(jsonl_lines)
 
 if __name__ == '__main__':
     main()
